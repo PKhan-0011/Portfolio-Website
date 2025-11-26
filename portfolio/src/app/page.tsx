1631c8b630713a2f14ed7e7cd6cab8761b613cb4
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-27 01:24:38 +0530

    code push

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,9 +3,11 @@ import { Container } from "@/components/container";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-start justify-start">
-      <Container className="lg:pd-8 min-h-screen p-4 md:p-6">
-        <h1 className="text-primary">Hi there!</h1>
-        <p>
+      <Container className="min-h-screen px-10 md:pt-20 md:pb-10">
+        <h1 className="text-3xl font-bold tracking-tight text-neutral-800 md:text-4xl dark:text-neutral-400">
+          Techy Prey
+        </h1>
+        <p className="max-w-lg pt-4 text-sm text-neutral-500 md:text-sm">
           Hi there i am sotfware enginner with a passoinate about how to code
           and!. learning something new daily and much more thing!..
         </p>
--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -97,3 +97,5 @@ export const Navbar = () => {
     </Container>
   );
 };
+
+// isme hai abhi kuch dikkat isko fix kario subha okkh!..
